4b74c2db43d93ada8e1e447169dfcd7109119cc6
GarvJaiswal <garvjaiswal08@gmail.com>
2024-01-29 12:10:09 +0530

    [Garv] Add:Added uc3

---
+++ AddressBookSystem/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        private string first_name;
+        private string last_name;
+        private string address;
+        private string city;
+        private string state;
+        private int zip;
+        private int PhoneNum;
+        private string email;
+
+        public Contact()
+        {
+        }
+        public Contact(string first, string last, string address, string city, string state, int zip, int phone_num, string email)
+        {
+            first_name = first;
+            last_name = last;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.PhoneNum = phone_num;
+            this.email = email;
+        }
+
+        public string FirstName
+        {
+            get { return first_name; } // Getter
+            set { first_name = value; } // Setter
+        }
+
+        public string LastName
+        {
+            get { return last_name; } // Getter
+            set { last_name = value; } // Setter
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public int Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public int PhoneNumber
+        {
+            get { return PhoneNum; }
+            set { PhoneNum = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,76 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AddressBookSystem
+namespace AddressBook
 {
-    public class Contact
+    class AddressBook
     {
-        string first_name;
-        string last_name;
-        string address;
-        string city;
-        string state;
-        int zip;
-        int phone_num;
-        string email;
-
-        public Contact() { 
-        }
-        public Contact(string first, string last, string address, string city, string state, int zip, int phone_num, string email)
+        class Contact
         {
-            first_name = first;
-            last_name = last;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.phone_num = phone_num;
-            this.email = email;
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string Zip { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
         }
 
-    }
-    public class AddressBook
-    {
-        public Contact record()
+        static void Main()
         {
-            
-            Console.WriteLine("Enter first name:");
-            string fname = Console.ReadLine();
-            Console.WriteLine("Enter last name:");
-            string lname = Console.ReadLine();
-            Console.WriteLine("Enter address:");
-            string addrs = Console.ReadLine();
-            Console.WriteLine("Enter city:");
-            string cname = Console.ReadLine();
-            Console.WriteLine("Enter state name:");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter zip code:");
-            int zipcode = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter phone num:");
-            int phone = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter email:");
-            string email = Console.ReadLine();
-            Console.ReadLine();
+            List<Contact> addressBook = new List<Contact>();
 
-            Contact obj = new Contact(fname,lname,addrs,cname,state,zipcode,phone,email);
-            return obj;
-
-        }
-        
-        static void Main(string[] args)
-        {
             Console.WriteLine("Welcome to Address Book");
 
-            List<Contact> list = new List<Contact>();
-            AddressBook newRecord= new AddressBook();
-            Contact details = new Contact();
-            newRecord.record();
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Add New Contact");
+                Console.WriteLine("2. Edit Existing Contact");
+                Console.WriteLine("3. Exit");
+
+                Console.Write("Enter your choice (1-3): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddNewContact(addressBook);
+                        break;
+                    case "2":
+                        EditExistingContact(addressBook);
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
+                        break;
+                }
+            }
+        }
+
+        static void AddNewContact(List<Contact> addressBook)
+        {
+            Contact newContact = new Contact();
+
+            Console.WriteLine("\nEnter Contact Details:");
+
+            Console.Write("Enter the First name: ");
+            newContact.FirstName = Console.ReadLine();
+
+            Console.Write("Enter the Last Name: ");
+            newContact.LastName = Console.ReadLine();
+
+            Console.Write("Enter the address: ");
+            newContact.Address = Console.ReadLine();
+
+            Console.Write("Enter the city: ");
+            newContact.City = Console.ReadLine();
+
+            Console.Write("Enter the zip: ");
+            newContact.Zip = Console.ReadLine();
+
+            Console.Write("Enter the email: ");
+            newContact.Email = Console.ReadLine();
+
+            Console.Write("Enter the phone number: ");
+            newContact.PhoneNumber = Console.ReadLine();
+
+            addressBook.Add(newContact);
+
+            Console.WriteLine("Contact added successfully.");
+            DisplayContactDetails(newContact);
+        }
+
+        static void EditExistingContact(List<Contact> addressBook)
+        {
+            Console.Write("Enter the First name of the contact to edit: ");
+            string firstNameToEdit = Console.ReadLine();
+
+            Contact existingContact = addressBook.Find(contact => contact.FirstName.Equals(firstNameToEdit, StringComparison.OrdinalIgnoreCase));
+
+            if (existingContact != null)
+            {
+                Console.WriteLine("Edit Contact Details:");
+
+                Console.Write("Enter the new First name: ");
+                existingContact.FirstName = Console.ReadLine();
+
+                Console.Write("Enter the new Last Name: ");
+                existingContact.LastName = Console.ReadLine();
+
+                Console.Write("Enter the new address: ");
+                existingContact.Address = Console.ReadLine();
+
+                Console.Write("Enter the new city: ");
+                existingContact.City = Console.ReadLine();
+
+                Console.Write("Enter the new zip: ");
+                existingContact.Zip = Console.ReadLine();
+
+                Console.Write("Enter the new email: ");
+                existingContact.Email = Console.ReadLine();
+
+                Console.Write("Enter the new phone number: ");
+                existingContact.PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Contact edited successfully.");
+                DisplayContactDetails(existingContact);
+            }
+            else
+            {
+                Console.WriteLine($"Contact with First Name '{firstNameToEdit}' not found.");
+            }
+        }
+
+        static void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine("\nContact Details:");
+            Console.WriteLine($"First Name: {contact.FirstName}");
+            Console.WriteLine($"Last Name: {contact.LastName}");
+            Console.WriteLine($"City: {contact.City}");
+            Console.WriteLine($"Zip: {contact.Zip}");
+            Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+            Console.WriteLine($"Email: {contact.Email}");
             Console.ReadLine();
-
-
         }
     }
 }
