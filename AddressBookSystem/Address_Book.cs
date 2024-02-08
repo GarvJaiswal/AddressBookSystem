bfbc3f8bb1c84787333b4880780f552dd212b32b
GarvJaiswal <garvjaiswal08@gmail.com>
2024-02-08 16:55:33 +0530

    [Garv] Add:Added new ability to read write using file I/O

--- AddressBookSystem/Address_Book.cs
+++ AddressBookSystem/Address_Book.cs
@@ -1,41 +1,60 @@
-﻿using System;
+﻿using abc;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 
-    public class AddressBook
+public class AddressBook
+{
+    private List<Contact> contacts = new List<Contact>();
+
+    public string Name { get; }
+
+    public AddressBook(string name)
     {
-        private List<Contact> contacts = new List<Contact>();
+        Name = name;
+    }
 
-        public string Name { get; }
+    public void AddContact(Contact contact)
+    {
+        contacts.Add(contact);
 
-        public AddressBook(string name)
+    }
+
+    public void DisplayContacts()
+    {
+
+        if (contacts.Count == 0)
         {
-            Name = name;
+            Console.WriteLine("No contacts in this Address Book.");
         }
-
-        public void AddContact(Contact contact)
+        else
         {
-            contacts.Add(contact);
-            
-        }
-
-        public void DisplayContacts()
-        {
-            
-            if (contacts.Count == 0)
+            Console.WriteLine($"Contacts in Address Book '{Name}':");
+            foreach (var contact in contacts)
             {
-                Console.WriteLine("No contacts in this Address Book.");
+                Console.WriteLine(contact);
+                Console.WriteLine();
             }
-            else
+        }
+    }
+
+    public void WriterToFile(string path)
+    {
+        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        {
+            using (StreamWriter writer = new StreamWriter(fs))
             {
-                Console.WriteLine($"Contacts in Address Book '{Name}':");
-                foreach (var contact in contacts)
+                foreach (var item in contacts)
                 {
-                    Console.WriteLine(contact);
-                    Console.WriteLine();
+                    writer.Write($"{item.FirstName},{item.LastName},{item.Address},{item.City},{item.State},{item.ZipCode},{item.PhoneNumber},{item.Email}");
+                    writer.WriteLine();
                 }
             }
         }
+        Console.WriteLine("Data written to file successfully");
+    }
+
 
 
         public void AddNewContact()
@@ -68,174 +87,213 @@ using System.Collections.Generic;
             contact.PhoneNumber = Console.ReadLine();
             Console.Write("Enter Email: ");
             contact.Email = Console.ReadLine();
+
             contacts.Add(contact);
         }
-        public void EditContact()
+        public Contact NewContactDetailsToWriteToFile()
         {
-            Console.Write("Enter First Name of the contact you want to edit: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter Last Name of the contact you want to edit: ");
-            string lastName = Console.ReadLine();
 
-            Contact existingContact = null;
-            foreach (Contact contact in contacts)
+
+            Contact contact = new Contact();
+            Console.Write("Enter First Name: ");
+            string FirstName = Console.ReadLine();
+
+            foreach (Contact detail in contacts)
             {
-                if (contact.FirstName == firstName && contact.LastName == lastName)
+                if (detail.FirstName == FirstName)
                 {
-                    existingContact = contact;
+                    Console.WriteLine("Duplicate entry");
+
+                }
+            }
+
+            contact.FirstName = FirstName;
+            Console.Write("Enter Last Name: ");
+            contact.LastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            contact.Address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            contact.City = Console.ReadLine();
+            Console.Write("Enter State: ");
+            contact.State = Console.ReadLine();
+            Console.Write("Enter Zip Code: ");
+            contact.ZipCode = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            contact.Email = Console.ReadLine();
+
+            return contact;
+        }
+   
+
+
+    public void EditContact()
+    {
+        Console.Write("Enter First Name of the contact you want to edit: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter Last Name of the contact you want to edit: ");
+        string lastName = Console.ReadLine();
+
+        Contact existingContact = null;
+        foreach (Contact contact in contacts)
+        {
+            if (contact.FirstName == firstName && contact.LastName == lastName)
+            {
+                existingContact = contact;
+                break;
+            }
+        }
+
+        if (existingContact != null)
+        {
+            Console.WriteLine("Enter\n1 to edit first name\n2 to edit last name\n3 to edit address\n4 to edit city\n5 to edit state\n6 to edit zipcode\n7 to edit phone number\n8 to edit email");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.Write("Enter new First Name: ");
+                    existingContact.FirstName = Console.ReadLine();
                     break;
-                }
-            }
-
-            if (existingContact != null)
-            {
-                Console.WriteLine("Enter\n1 to edit first name\n2 to edit last name\n3 to edit address\n4 to edit city\n5 to edit state\n6 to edit zipcode\n7 to edit phone number\n8 to edit email");
-                int option = Convert.ToInt32(Console.ReadLine());
-
-                switch (option)
-                {
-                    case 1:
-                        Console.Write("Enter new First Name: ");
-                        existingContact.FirstName = Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.Write("Enter new Last Name: ");
-                        existingContact.LastName = Console.ReadLine();
-                        break;
-                    case 3:
-                        Console.Write("Enter new Address: ");
-                        existingContact.Address = Console.ReadLine();
-                        break;
-                    case 4:
-                        Console.Write("Enter new City: ");
-                        existingContact.City = Console.ReadLine();
-                        break;
-                    case 5:
-                        Console.Write("Enter new State: ");
-                        existingContact.State = Console.ReadLine();
-                        break;
-                    case 6:
-                        Console.Write("Enter new Zip Code: ");
-                        existingContact.ZipCode = Console.ReadLine();
-                        break;
-                    case 7:
-                        Console.Write("Enter new Phone Number: ");
-                        existingContact.PhoneNumber = Console.ReadLine();
-                        break;
-                    case 8:
-                        Console.Write("Enter new Email: ");
-                        existingContact.Email = Console.ReadLine();
-                        break;
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("Contact not found.");
-            }
-        }
-
-        public void DeleteContact()
-        {
-            Console.Write("Enter First Name of the contact you want to delete: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter Last Name of the contact you want to delete: ");
-            string lastName = Console.ReadLine();
-
-            Contact contactToRemove = null;
-            foreach (Contact contact in contacts)
-            {
-                if (contact.FirstName == firstName && contact.LastName == lastName)
-                {
-                    contactToRemove = contact;
+                case 2:
+                    Console.Write("Enter new Last Name: ");
+                    existingContact.LastName = Console.ReadLine();
                     break;
-                }
+                case 3:
+                    Console.Write("Enter new Address: ");
+                    existingContact.Address = Console.ReadLine();
+                    break;
+                case 4:
+                    Console.Write("Enter new City: ");
+                    existingContact.City = Console.ReadLine();
+                    break;
+                case 5:
+                    Console.Write("Enter new State: ");
+                    existingContact.State = Console.ReadLine();
+                    break;
+                case 6:
+                    Console.Write("Enter new Zip Code: ");
+                    existingContact.ZipCode = Console.ReadLine();
+                    break;
+                case 7:
+                    Console.Write("Enter new Phone Number: ");
+                    existingContact.PhoneNumber = Console.ReadLine();
+                    break;
+                case 8:
+                    Console.Write("Enter new Email: ");
+                    existingContact.Email = Console.ReadLine();
+                    break;
             }
 
-            if (contactToRemove != null)
-            {
-                contacts.Remove(contactToRemove);
-                Console.WriteLine($"Contact {firstName} {lastName} deleted successfully.");
-            }
-            else
-            {
-                Console.WriteLine("Contact not found.");
-            }
         }
-
-        public List<Contact> SearchInCityOrState(string name, string city, string state)
+        else
         {
-            List<Contact> foundContacts = new List<Contact>();
-            string nameLower = name.ToLower();
-            string cityLower = city.ToLower();
-            string stateLower = state.ToLower();
-            foreach (var contact in contacts)
+            Console.WriteLine("Contact not found.");
+        }
+    }
+
+    public void DeleteContact()
+    {
+        Console.Write("Enter First Name of the contact you want to delete: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter Last Name of the contact you want to delete: ");
+        string lastName = Console.ReadLine();
+
+        Contact contactToRemove = null;
+        foreach (Contact contact in contacts)
+        {
+            if (contact.FirstName == firstName && contact.LastName == lastName)
             {
-                string Namelower = contact.FirstName.ToLower();
-                string Citylower = city.ToLower();
-                string Statelower = state.ToLower();
-                if (Namelower.Equals(nameLower) && (Citylower.Equals(cityLower) || Statelower.Equals(stateLower)))
-                {
-                    foundContacts.Add(contact);
-                }
+                contactToRemove = contact;
+                break;
             }
-            return foundContacts;
         }
 
-        public List<Contact> SearchByCity(string cityName)
+        if (contactToRemove != null)
         {
-            List<Contact> foundContacts = new List<Contact>();
-            string cityLower = cityName.ToLower();
-            foreach (var contact in contacts)
-            {
-                if (contact.City.ToLower() == cityLower)
-                {
-                    foundContacts.Add(contact);
-                }
-            }
-            return foundContacts;
+            contacts.Remove(contactToRemove);
+            Console.WriteLine($"Contact {firstName} {lastName} deleted successfully.");
         }
+        else
+        {
+            Console.WriteLine("Contact not found.");
+        }
+    }
 
-        public List<Contact> SearchByState(string stateName)
+    public List<Contact> SearchInCityOrState(string name, string city, string state)
+    {
+        List<Contact> foundContacts = new List<Contact>();
+        string nameLower = name.ToLower();
+        string cityLower = city.ToLower();
+        string stateLower = state.ToLower();
+        foreach (var contact in contacts)
         {
-            List<Contact> foundContacts = new List<Contact>();
-            string stateLower = stateName.ToLower();
-            foreach (var contact in contacts)
+            string Namelower = contact.FirstName.ToLower();
+            string Citylower = city.ToLower();
+            string Statelower = state.ToLower();
+            if (Namelower.Equals(nameLower) && (Citylower.Equals(cityLower) || Statelower.Equals(stateLower)))
             {
-                if (contact.State.ToLower() == stateLower)
-                {
-                    foundContacts.Add(contact);
-                }
+                foundContacts.Add(contact);
             }
-            return foundContacts;
         }
-      
+        return foundContacts;
+    }
 
-       public List<Contact> CountByCityName(string cityName)
+    public List<Contact> SearchByCity(string cityName)
+    {
+        List<Contact> foundContacts = new List<Contact>();
+        string cityLower = cityName.ToLower();
+        foreach (var contact in contacts)
         {
-            List<Contact> foundContacts = new List<Contact>();
-            string cName = cityName.ToLower();
-            foreach (var contact in contacts)
+            if (contact.City.ToLower() == cityLower)
             {
-                if (contact.City.ToLower().Equals(cName))
-                { foundContacts.Add(contact); }
+                foundContacts.Add(contact);
             }
-            return foundContacts;
         }
+        return foundContacts;
+    }
 
-        public List<Contact> CountByStateName(string stateName)
+    public List<Contact> SearchByState(string stateName)
+    {
+        List<Contact> foundContacts = new List<Contact>();
+        string stateLower = stateName.ToLower();
+        foreach (var contact in contacts)
         {
-            List<Contact> foundContacts = new List<Contact>();
-            string sName = stateName.ToLower();
-            foreach (var contact in contacts)
+            if (contact.State.ToLower() == stateLower)
             {
-                if (contact.State.ToLower().Equals(sName))
-                {
-                    foundContacts.Add(contact);
-                }
+                foundContacts.Add(contact);
             }
-            return foundContacts;
         }
+        return foundContacts;
+    }
+
+
+    public List<Contact> CountByCityName(string cityName)
+    {
+        List<Contact> foundContacts = new List<Contact>();
+        string cName = cityName.ToLower();
+        foreach (var contact in contacts)
+        {
+            if (contact.City.ToLower().Equals(cName))
+            { foundContacts.Add(contact); }
+        }
+        return foundContacts;
+    }
+
+    public List<Contact> CountByStateName(string stateName)
+    {
+        List<Contact> foundContacts = new List<Contact>();
+        string sName = stateName.ToLower();
+        foreach (var contact in contacts)
+        {
+            if (contact.State.ToLower().Equals(sName))
+            {
+                foundContacts.Add(contact);
+            }
+        }
+        return foundContacts;
+    }
     public void SortContactsByName()
     {
         for (int i = 0; i < contacts.Count - 1; i++)
@@ -244,7 +302,7 @@ using System.Collections.Generic;
             {
                 if (string.Compare(contacts[j].FirstName, contacts[j + 1].FirstName) > 0)
                 {
-                    
+
                     Contact temp = contacts[j];
                     contacts[j] = contacts[j + 1];
                     contacts[j + 1] = temp;
@@ -254,9 +312,9 @@ using System.Collections.Generic;
     }
     public void SortContactsByCity()
     {
-        for(int i = 0;i < contacts.Count - 1;i++)
+        for (int i = 0; i < contacts.Count - 1; i++)
         {
-            for(int j=0;j < contacts.Count -i - 1; j++)
+            for (int j = 0; j < contacts.Count - i - 1; j++)
             {
                 if (string.Compare(contacts[j].City, contacts[j + 1].City) > 0)
                 {
@@ -300,5 +358,4 @@ using System.Collections.Generic;
             }
         }
     }
-
 }
